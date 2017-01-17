f0a26c3bfde7cab5a2baacfe46af3ab6d1b7f545
PKMT206 <PKMT206@MGTECHWKS14.MGTECHSOFT.IN>
2017-01-17 16:48:51 +0530

    Initial changes to GIT Repository.

---
+++ mgen/src/main/java/com/shopping/mgen/App.java
@@ -0,0 +1,13 @@
+package com.shopping.mgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
